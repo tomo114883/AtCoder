6be62956c781e69c96c768607a052fbf05cb2033
TaigaTomonaga <taigatomonaga@gmail.com>
2024-09-15 10:27:52 +0900

    incorrect

--- ts/src/B_Piano3.ts
+++ ts/src/B_Piano3.ts
@@ -1,4 +1,4 @@
-type date = {
+type PushKey = {
   a: number,
   s: string,
 };
@@ -7,37 +7,34 @@ const piano3 = (input: string) => {
   //TODO: Receive inputs.
   const lines: string[] = input.split("\n");
   const n = parseInt(lines[0]);
-  const items: date[] = lines.map((line) => {
+  const items: PushKey[] = lines.splice(1, n + 1).map((line) => {
     const [a, s] = line.split(" ");
     return { a: parseInt(a), s: s };
   });
+  
+  console.log(items)
   //TODO: Process the calculations each L and R.
   // Iterate N times, and calculate each hands's fatigue level.
-  let initL = items.find(item => item.s === "L");
-  let initR = items.find(item => item.s === "R");
+  // let initL = items.find(item => item.s === "L");
+  // let initR = items.find(item => item.s === "R");
   // console.log(initL)
-  let absL = initL?.a ?? 0;
+  // let absL = initL?.a ?? 0;
   // console.log(`re: ${resultL}`)
-  let absR = initR?.a ?? 0;
-  let beforeL = 0;
-  let beforeR = 0;
-  let  resultL = 0;
-  let  resultR = 0;
+  // let absR = initR?.a ?? 0;
+  let itemsL: number[] = [];
+  let itemsR: number[] = [];
 
-  //!! Use reduce()
+  // Reassignment each itemsL and itemsR from items.
   for (let i = 0; i < n; i++) {
     if (items[i].s === "L") {
-      absL = Math.abs(items[i].a - beforeL);
-      resultL += absL;
-      beforeL = items[i].a;
-      // console.log(resultL)
-    }
-    if (items[i].s === "R") {
-      absR = Math.abs(items[i].a - beforeR);
-      resultR += absR;
-      beforeR = items[i].a;
+      itemsL.push(items[i].a);
+    } else {
+      itemsR.push(items[i].a);
     }
   }
+
+  const resultL = itemsL.reduce((pre, curr) => Math.abs(curr - pre), 0);
+  const resultR = itemsR.reduce((pre, curr) => Math.abs(curr - pre), 0);
 
   const result = resultL + resultR;
   //TODO: Output the minimum fatigue level.
