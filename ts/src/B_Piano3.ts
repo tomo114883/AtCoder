997176277972697cfbf9a8c1581b8ee2a49b91c9
TaigaTomonaga <taigatomonaga@gmail.com>
2024-09-15 11:40:56 +0900

    clear

--- ts/src/B_Piano3.ts
+++ ts/src/B_Piano3.ts
@@ -1,44 +1,31 @@
-type PushKey = {
-  a: number,
-  s: string,
-};
-
 const piano3 = (input: string) => {
   //TODO: Receive inputs.
-  const lines: string[] = input.split("\n");
+  const lines = input.split("\n");
   const n = parseInt(lines[0]);
-  const items: PushKey[] = lines.splice(1, n + 1).map((line) => {
+  const items = lines.splice(1, n).map(line => {
     const [a, s] = line.split(" ");
-    return { a: parseInt(a), s: s };
+    return { a: parseInt(a), s: s};
   });
-  
-  console.log(items)
-  //TODO: Process the calculations each L and R.
-  // Iterate N times, and calculate each hands's fatigue level.
-  // let initL = items.find(item => item.s === "L");
-  // let initR = items.find(item => item.s === "R");
-  // console.log(initL)
-  // let absL = initL?.a ?? 0;
-  // console.log(`re: ${resultL}`)
-  // let absR = initR?.a ?? 0;
-  let itemsL: number[] = [];
-  let itemsR: number[] = [];
+  // console.log(items)
 
-  // Reassignment each itemsL and itemsR from items.
+  //TODO: Process the calculations each L and R.
+  let pos = [-1, -1]; // position of hand
+  let fatigueLev = 0; // ans as the minimum fatigue level
+  
   for (let i = 0; i < n; i++) {
-    if (items[i].s === "L") {
-      itemsL.push(items[i].a);
-    } else {
-      itemsR.push(items[i].a);
+    const { a, s } = items[i];
+    const hand = (s === "L") ? 0 : 1; // Define which hand is moved.
+
+    // Calculate the fatigue level at this time.
+    if (pos[hand] !== -1) {
+      fatigueLev += Math.abs(a - pos[hand]);
     }
+    
+    pos[hand] = a; // Move the hand.
   }
 
-  const resultL = itemsL.reduce((pre, curr) => Math.abs(curr - pre), 0);
-  const resultR = itemsR.reduce((pre, curr) => Math.abs(curr - pre), 0);
-
-  const result = resultL + resultR;
-  //TODO: Output the minimum fatigue level.
-  console.log(result);
+  //TODO: Output the minimum fatigue level
+  console.log(fatigueLev)
 }
 
 piano3(require("fs").readFileSync("/dev/stdin", "utf8"));
