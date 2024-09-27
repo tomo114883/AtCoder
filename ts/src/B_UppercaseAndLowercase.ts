6f7409076eb4c0b02996dbb9032e751ad4f65bb9
TaigaTomonaga <taigatomonaga@gmail.com>
2024-09-27 11:16:48 +0900

    refactor B_UppercaseAndLowercase

--- ts/src/B_UppercaseAndLowercase.ts
+++ ts/src/B_UppercaseAndLowercase.ts
@@ -1,23 +1,11 @@
 const uppercaseAndLowercase = (input: string) => {
-  // TODO: Receive input.
-  const s: string[] = input.split('');
-  let ans: string;
-  let cntUpper: number = 0;
-  let cntLower: number = 0;
   // TODO: Process here.
-  // Determine if the each s is uppercase or lowercase.
-  s.map((value) => {
-    if (value === value.toUpperCase()) {
-      cntUpper++;
-    } else {
-      cntLower++;
-    }
-  });
-  if (cntUpper > cntLower) {
-    ans = input.toUpperCase();
-  } else {
-    ans = input.toLowerCase();
-  }
+  const upperCount = input.replace(/[^A-Z]/g, '').length;
+  const lowerCount = input.replace(/[^a-z]/g, '').length;
+  console.log(upperCount, lowerCount);
+  // Determine which count is greater.
+  const ans =
+    upperCount > lowerCount ? input.toUpperCase() : input.toLowerCase();
   // TODO: Output ans.
   console.log(ans);
 };
