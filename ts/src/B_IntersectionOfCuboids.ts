fae3fb747fc76ce9fca49e8c23ac34d938714937
TaigaTomonaga <taigatomonaga@gmail.com>
2024-09-29 10:54:04 +0900

    refactor B_IntersectionOfCuboids

--- ts/src/B_IntersectionOfCuboids.ts
+++ ts/src/B_IntersectionOfCuboids.ts
@@ -1,19 +1,28 @@
 const intersectionOfCuboids = (input: string) => {
   // TODO: Receive input.
-  const lines = input.split(/\n/);
-  const [a, b, c, d, e, f] = lines[0].split(' ').map(Number);
-  const [g, h, i, j, k, l] = lines[1].split(' ').map(Number);
+  const lines = input.trim().split(/\n/);
+  const [x1, y1, z1, x2, y2, z2] = lines[0].split(' ').map(Number);
+  const [x3, y3, z3, x4, y4, z4] = lines[1].split(' ').map(Number);
   // TODO: Process here.
-  let ans = false;
-  let ans1 = true;
-  let ans2 = true;
-  let ans3 = true;
-  if (a >= j || d <= g) ans1 = false;
-  if (b >= k || e <= h) ans2 = false;
-  if (c >= l || f <= i) ans3 = false;
-  if (ans1 && ans2 && ans3) ans = true;
+  let ans = '';
+  const determineIntersection = (
+    l1: number,
+    r1: number,
+    l2: number,
+    r2: number,
+  ) => {
+    if (l1 >= r2 || l2 >= r1) return false;
+    return true;
+  };
+
+  ans =
+    determineIntersection(x1, x2, x3, x4) &&
+    determineIntersection(y1, y2, y3, y4) &&
+    determineIntersection(z1, z2, z3, z4)
+      ? 'Yes'
+      : 'No';
   // TODO: Output ans.
-  console.log(ans ? 'Yes' : 'No');
+  console.log(ans);
 };
 
 intersectionOfCuboids(require('fs').readFileSync('/dev/stdin', 'utf8'));
