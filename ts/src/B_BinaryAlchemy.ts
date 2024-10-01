aa9ac96fd6480ba0b4589beb612120d45a712fc5
TaigaTomonaga <taigatomonaga@gmail.com>
2024-10-01 11:07:04 +0900

    refactor B_BinaryAlchemy

--- ts/src/B_BinaryAlchemy.ts
+++ ts/src/B_BinaryAlchemy.ts
@@ -1,31 +1,23 @@
-function binaryAlchemy(input: string): void {
-  // Split inputs into new lines and get n.
-  const lines = input.split('\n');
-  const n = parseInt(lines[0]);  // number of elements
-
-  // Create 2D array by reading a[i][j].
-  const a: number[][] = [];
-  let lineIndex = 1;
-  for (let i = 0; i < n; i++) {
-    a.push(lines[lineIndex].split(' ').map(Number));
-    lineIndex++;
-  }
-  console.log(`${a}`)
-
-  let result = 1; // the initial element
-
+const binaryAlchemy = (input: string) => {
+  // Receive input as a 2D array.
+  const lines: string[] = input.trim().split(/\n/);
+  const n = parseInt(lines[0]);
+  const elements: number[][] = lines
+    .slice(1)
+    .map((line) => line.split(' ').map(Number));
   // Combine elements.
-  for (let j = 1; j <= n; j++) {
-    //!! There is a 1-point discrepancy between the elements number and the index.
-    if (result >= j) {
-      result = a[result - 1][j - 1];  // if i≥j, i and j transform into element a[i][j]j 
+  let i = 1;
+  for (let j = 0; j < n; j++) {
+    if (i - 1 >= j) {
+      // i >= j
+      i = elements[i - 1][j]; // Create a new element.
     } else {
-      result = a[j - 1][result - 1];  // if i<j, i and j transform into element a[j][i]j 
+      // i < j
+      i = elements[j][i - 1]; // Create a new element.
     }
   }
+  // Output ans as a final element.
+  console.log(`${i}`);
+};
 
-  // Output final result.
-  console.log(result);
-}
-
-binaryAlchemy(require("fs").readFileSync("/dev/stdin", "utf8"));
+binaryAlchemy(require('fs').readFileSync('/dev/stdin', 'utf8'));
