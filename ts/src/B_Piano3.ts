3114125e8adfda48cd0de6fc86058b25460e1aa1
TaigaTomonaga <taigatomonaga@gmail.com>
2024-10-26 10:43:14 +0900

    refactor B_Piano3

--- ts/src/B_Piano3.ts
+++ ts/src/B_Piano3.ts
@@ -1,42 +1,24 @@
 const piano3 = (input: string) => {
   // Receive input.
-  const lines = input.split(/\n/);
+  const lines = input.trim().split(/\n/);
   const instructions: { pos: number; hand: string }[] = lines
     .splice(1)
     .map((line) => {
       const [pos, hand] = line.split(/ /);
       return { pos: parseInt(pos), hand: hand };
     });
-  // Init a fatigue level.
-  let [fatigueForL, fatigueForR] = [0, 0];
-  let [posOfL, posOfR] = [0, 0];
+  // Init each current positions and the fatigue level.
+  let currPos = [-1, -1];
   let fatigue = 0;
   // Calculate a fatigue level.
   instructions.map((instruction) => {
     const { pos, hand } = instruction;
-    if (hand === 'L') {
-      // Calculate for a left hand.
-      if (fatigueForL === 0) {
-        posOfL = pos;
-        fatigueForL = pos;
-      } else if (pos === posOfL) {
-      } else {
-        fatigueForL = Math.abs(pos - posOfL);
-        fatigue += fatigueForL;
-        posOfL = pos;
-      }
-    } else {
-      // Calculate for a right hand.
-      if (fatigueForR === 0) {
-        posOfR = pos;
-        fatigueForR = pos;
-      } else if (pos === posOfR) {
-      } else {
-        fatigueForR = Math.abs(pos - posOfR);
-        fatigue += fatigueForR;
-        posOfR = pos;
-      }
+    const side = hand === 'L' ? 0 : 1;
+
+    if (currPos[side] !== -1) {
+      fatigue += Math.abs(pos - currPos[side]);
     }
+    currPos[side] = pos;
   });
   // Output ans as a fatigue level for a performance.
   console.log(fatigue);
